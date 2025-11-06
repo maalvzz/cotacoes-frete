cd294da35f2e7a95c99be21000991f444b2805d6
I.R. COMERCIO E MATERIAIS ELETRICOS LTDA <miguel.alves1@faculdadegran.edu.br>
2025-11-05 21:50:18 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // ✅ Altere para a URL real do seu Portal Central
 const PORTAL_URL = 'https://ir-comercio-portal-zcan.onrender.com';
-const API_URL = 'https://cotacoes-frete.onrender.com/api/cotacoes';
+const API_URL = 'https://cotacoes-frete-aikc.onrender.com/api/cotacoes';
 
 const STORAGE_KEY = 'cotacoes_frete';
 const POLLING_INTERVAL = 10000;
